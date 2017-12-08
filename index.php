4e9b6b48f1d79e32620431f974a3d3b87960b6ed
Rain Kütt <rain.kutt@khk.ee>
2017-12-08 09:27:42 +0200

    kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rain Kütt
+ * Date: 08.12.2017
+ * Time: 9:03
+ */
+
+// kommentaar
+# kah üherealine kommentaar
+/*
+ * miturida kommentaare
+ */
+
+?>
+
