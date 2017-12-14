68728fadfed27fd546c8c5fb36e3e5c0fd59b5fb
Rain Kütt <rain.kutt@khk.ee>
2017-12-14 14:29:20 +0200

    käsitletud while ja do while tsüklid ja break

--- numbrid.php
+++ numbrid.php
@@ -16,6 +16,7 @@ for ($arv=1; $arv <= 10; $arv++) {
     echo '<font color="'.$varv.'">'.$arv.'</font><br />';
 }
 //
+
 $a=5; $b=2;
 $c=$a++;
 $d=$b--;
@@ -23,6 +24,23 @@ echo 'a= '.$a.'<br />';
 echo 'b= '.$b.'<br />';
 echo 'c= '.$c.'<br />';
 echo 'd= '.$d.'<br />';
+//
+
+$kord=1;
+while($kord <= 5) {
+    echo '<i>'.$kord++.'</i><br />';
+    if($kord==3){
+        break;
+    }
+}
+//
+$kord=1;
+do {
+    if($kord==3){
+        continue;
+    }
+    echo '<b>'.$kord.'</b><br />';
+} while(++$kord <= 5);
 
 ?>
 
