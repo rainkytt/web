efa0de52186549ecfba97984f180baa7923cf3e0
Rain Kütt <rain.kutt@khk.ee>
2017-12-14 13:33:36 +0200

    värvilised numbrid

--- numbrid.php
+++ numbrid.php
@@ -5,8 +5,16 @@
  * Date: 14.12.2017
  * Time: 12:13
  */
-echo "<font color=\"red\">1 3 5 7 9</font> \n";
-echo "<font color=\"blue\">2 4 6 8 10</font>";
+/*echo "<font color=\"red\">1 3 5 7 9</font> \n";
+echo "<font color=\"blue\">2 4 6 8 10</font>";*/
+for ($arv=1; $arv <= 10; $arv++) {
+    if($arv % 2 == 0) {
+        $varv = 'blue';
+    } else {
+        $varv = 'red';
+    }
+    echo '<font color="'.$varv.'">'.$arv.'</font><br />';
+}
 ?>
 
 
