5c4e4f1603953eb4588535d18635e34cac64a07b
Rain Kütt <rain.kutt@khk.ee>
2017-12-21 14:51:37 +0200

    ül1 lahendatud

--- massiivid.php
+++ massiivid.php
@@ -27,3 +27,31 @@ for($i=0; $i < count($arvud); $i++){
 foreach ($arvud1 as $arv){
     echo '<i>'.$arv.'</i><br />';
 }
+
+echo '<hr />';
+
+/*
+ * 1.
+ * Koosta funktsioon nimega looMassiiv,
+ * mis võtab parameetrina massiivi elementide
+ * arvu ja loob juhuarvudest (täisarvud)
+ * vahemikust 100-999 koosneva massiivi.
+ * Loodud massiiv tuleb tagastada
+ * põhiprogrammile ja kontrollida sisu
+ * testväljastuse abil - näiteks print_r
+ * */
+function looMassiiv($elementideArv){
+    $massiiv = array();
+    for ($kord=1; $kord <= $elementideArv; $kord++){
+        $juhuarv = rand(100, 999);
+        $massiiv[] = $juhuarv;
+    }
+    return $massiiv;
+}
+
+$testMassiiv = looMassiiv(5);
+echo '<pre>';
+print_r($testMassiiv);
+echo '</pre>';
+echo '<hr />';
+
