09ae56ca45165de685610c08b9c02db158108ee8
Rain Kütt <rain.kutt@khk.ee>
2017-12-21 14:34:24 +0200

    massiivi korralik väljastus

--- massiivid.php
+++ massiivid.php
@@ -13,4 +13,17 @@ $arvud[] = 4;
 $arvud[] = 2;
 $arvud[] = 3;
 
-// väärtustega massiiv $arvud1 = [1,2,3,4,5];
+// väärtustega massiiv
+$arvud1 = [1,2,3,4,5];
+echo '<pre>';
+print_r($arvud);
+echo '</pre>';
+
+// korralik väljastus
+for($i=0; $i < count($arvud); $i++){
+    echo '<b>'.$arvud[$i].'</b><br />';
+}
+
+foreach ($arvud1 as $arv){
+    echo '<i>'.$arv.'</i><br />';
+}
