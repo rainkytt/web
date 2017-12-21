4d1838aa1c0eacfbe41f7fda99009e85237ec28e
Rain Kütt <rain.kutt@khk.ee>
2017-12-21 14:23:38 +0200

    tühja massiivi loomine ja väärtuste lisamine

---
+++ massiivid.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rain Kütt
+ * Date: 21.12.2017
+ * Time: 14:20
+ */
+$arvud = array(); // tühi massiiv
+// lisame väärtused
+$arvud[] = 5;
+$arvud[] = 1;
+$arvud[] = 4;
+$arvud[] = 2;
+$arvud[] = 3;
+
+// väärtustega massiiv $arvud1 = [1,2,3,4,5];
