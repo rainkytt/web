9c40c143c798f8d50a5bb5ab4dee281d2cc7f5b4
Rain Kütt <rain.kutt@khk.ee>
2018-01-25 11:06:27 +0200

    muudetud konstruktori järgi

--- tekstiTest.php
+++ tekstiTest.php
@@ -8,9 +8,10 @@
 require_once 'tekst.php';
 
 // loome teksti objekti
-$minuTekst = new tekst();
+// $minuTekst = new tekst();
 // määrame tekstile sisu
-$minuTekst->maaraTekst('Tere maailm');
+// $minuTekst->maaraTekst('Tere maailm');
+$minuTekst = new tekst('Tere maailm!');
 // vaatame, mis on objekti sees
 echo '<pre>';
 print_r($minuTekst);
