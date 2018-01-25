9ef17283314fc36e43a9a50133ca3655cf78cb7b
Rain Kütt <rain.kutt@khk.ee>
2018-01-25 11:39:17 +0200

    kah punane värv tekstile lisatud

--- tekstiTest.php
+++ tekstiTest.php
@@ -18,3 +18,22 @@ print_r($minuTekst);
 echo '</pre>';
 // prindime välja
 $minuTekst->prindiTekst();
+
+// loome veel ühe objekti
+// ja määrame sisu
+$mustTekst = new tekst('Must tekst');
+// vaatame, mis on objekti sees
+echo '<pre>';
+print_r($mustTekst);
+echo '</pre>';
+// prindime välja
+$mustTekst->prindiTekst();
+
+require_once 'vTekst.php';
+// loome värvilise objekti ja määrame sisu
+$punaneTekst = new vTekst('Punane tekst','#ff0000');
+echo '<pre>';
+print_r($punaneTekst);
+echo '</pre>';
+// prindime välja
+$punaneTekst->prindiTekst();
