b069d034760a4084f55f53785e4751652c114cbf
Rain Kütt <rain.kutt@khk.ee>
2018-01-25 10:56:32 +0200

    loodud tekstiobjekt ja kontrollitud tema töö

---
+++ tekstiTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rain Kütt
+ * Date: 25.01.2018
+ * Time: 10:47
+ */
+require_once 'tekst.php';
+
+// loome teksti objekti
+$minuTekst = new tekst();
+// määrame tekstile sisu
+$minuTekst->maaraTekst('Tere maailm');
+// vaatame, mis on objekti sees
+echo '<pre>';
+print_r($minuTekst);
+echo '</pre>';
+// prindime välja
+$minuTekst->prindiTekst();
